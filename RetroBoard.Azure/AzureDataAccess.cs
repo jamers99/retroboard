054f2d53ae9ec5d2328827f2fe476d54fe262ffe
DanielGlick45 <danielglck51@gmail.com>
2023-10-13 14:35:37 -0400

    Set up work item client

--- RetroBoard.Azure/AzureDataAccess.cs
+++ RetroBoard.Azure/AzureDataAccess.cs
@@ -1,12 +1,31 @@
-﻿namespace RetroBoard.Azure;
+﻿using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+
+namespace RetroBoard.Azure;
 
 public class AzureDataAccess : IDataAccess
 {
+    WorkItemTrackingHttpClient workItemClient;
+    WorkItemTrackingHttpClient WorkItemClient
+    {
+        get
+        {
+            if (workItemClient == null)
+            {
+                var vssConnection = new VssConnection(new Uri(settings.Url), new VssBasicCredential("pat", settings.Pat));
+                workItemClient = vssConnection.GetClient<WorkItemTrackingHttpClient>();
+            }
+
+            return workItemClient;
+        }
+    }
+
     AzureSettings settings;
     public AzureDataAccess(AzureSettings azureSettings)
     {
         settings = azureSettings;
     }
- 
+
     public string Name => settings.Url;
 }
