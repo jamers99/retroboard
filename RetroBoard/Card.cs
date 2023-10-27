ab5d12629987046e985aa9e36472809e15155d0c
James Esh <james@koblesystems.com>
2023-10-27 15:22:02 -0400

    dynamic color

--- RetroBoard/Card.cs
+++ RetroBoard/Card.cs
@@ -6,4 +6,10 @@ public class Card
     public string? Title { get; init; }
     public CardType Type { get; init; }
     public bool IsBlocked { get; init; }
+    public string Color => Type switch
+    {
+        CardType.Bug => "rgba(120, 30, 20, 0.5)",
+        CardType.PBI => "rgba(30, 30, 120, 0.5)",
+        _ => "rgba(200, 200, 200, 0.5)"
+    };
 }
